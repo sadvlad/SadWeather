a697c18b5d33ffe3b1520cebd29b71546f43aea1
sadvlad <40368071+sadvlad@users.noreply.github.com>
2020-11-10 17:53:37 +0300

    add app environment

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/, /dist/],
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './index.html',
+      filename: 'index.html'
+    }),
+    new ESLintPlugin({
+      extensions: ['js']
+    })
+  ]
+};
