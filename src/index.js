2552556a6c1bde06fb3bbf000081721c4cd27cc6
sadvlad <40368071+sadvlad@users.noreply.github.com>
2020-11-19 18:08:19 +0300

    add coordinates, more css, search

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import CSS from './main.css';
 
+async function getLocation() {
+  const urlLoc = 'https://ipinfo.io/json?token=eb5b90bb77d46a';
+  const locResponce = await fetch(urlLoc);
+  const locData = await locResponce.json();
+  const location = await locData.loc;
+  localStorage.setItem('city', location);
+  return location;
+}
+
 const rusWeekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 const engWeekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const rusText = ['Поиск', 'ощущается как', 'скорость ветра', 'км/ч', 'влажность'];
-const engText = ['Search', 'Feels like', 'wind speed', 'kph', 'humidity'];
+const rusText = ['Поиск', 'Ощущается как', 'Скорость ветра', 'км/ч', 'Влажность', 'Широта', 'Долгота'];
+const engText = ['Search', 'Feels like', 'Wind speed', 'kph', 'Humidity', 'Latitude', 'Longitude'];
 const selectLang = document.querySelector('.selectLang');
 const selectMetr = document.querySelector('.selectMetr');
+const searchInp = document.querySelector('.controlsSearchInp');
+// const searchForm = document.querySelector('.controlsSearch');
+const searchBtn = document.querySelector('.controlsSearchBtn');
 let lang = localStorage.getItem('lang');
 let metr = localStorage.getItem('metr');
+let cityRequest = localStorage.getItem('city');
 
 if (lang === 'ru') {
   selectLang.selectedIndex = 1;
@@ -19,15 +32,29 @@ if (metr === 'f') {
 } else if (metr === 'c') {
   selectMetr.selectedIndex = 0;
 }
+
+searchBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  cityRequest = searchInp.value;
+  if (selectLang.value === 'ru') {
+    getApis(cityRequest, rusText, rusWeekDays);
+    getMap(getCoord(cityRequest), lang);
+  } else if (selectLang.value === 'en') {
+    getApis(cityRequest, engText, engWeekDays);
+    getMap(getCoord(cityRequest), lang);
+  }
+  searchInp.value = '';
+});
+
 selectLang.addEventListener('change', () => {
   localStorage.setItem('lang', `${selectLang.value}`);
   lang = selectLang.value;
   if (selectLang.value === 'ru') {
-    getApis(rusText, rusWeekDays);
-    getMap(getCoord(), lang);
+    getApis(cityRequest, rusText, rusWeekDays);
+    getMap(getCoord(cityRequest), lang);
   } else if (selectLang.value === 'en') {
-    getApis(engText, engWeekDays);
-    getMap(getCoord(), lang);
+    getApis(cityRequest, engText, engWeekDays);
+    getMap(getCoord(cityRequest), lang);
   }
 }, false);
 
@@ -35,9 +62,9 @@ selectMetr.addEventListener('change', () => {
   localStorage.setItem('metr', `${selectMetr.value}`);
   metr = selectMetr.value;
   if (selectLang.value === 'ru') {
-    getApis(rusText, rusWeekDays);
+    getApis(cityRequest, rusText, rusWeekDays);
   } else if (selectLang.value === 'en') {
-    getApis(engText, engWeekDays);
+    getApis(cityRequest, engText, engWeekDays);
   }
 });
 
@@ -69,13 +96,10 @@ controlsBackgroundBtn.addEventListener('click', () => {
   getAndChangeFon();
 });
 
-async function getApis(text, WeekDays) {
-  const urlLoc = 'https://ipinfo.io/json?token=eb5b90bb77d46a';
+async function getApis(searchOptions, text, WeekDays) {
   const apiKey = 'dbdff77127fc4281abb81651200911';
-  const locResponce = await fetch(urlLoc);
-  const locData = await locResponce.json();
-  let searhOptions = await locData.loc;
-  const urlWeather = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searhOptions}&days=3&lang=${lang}`;
+  const urlWeather = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${searchOptions}&days=3&lang=${lang}`;
+
   const weatherResponce = await fetch(urlWeather);
   const weatherData = await weatherResponce.json();
 
@@ -136,17 +160,27 @@ async function getApis(text, WeekDays) {
 
   thirdWeekDay.innerHTML = WeekDays[(isDay + 2)];
   thirdDayIcon.src = `./icons/${weatherData.forecast.forecastday[2].day.condition.icon.slice(34)}`;
-}
-if (lang === 'ru') {
-  getApis(rusText, rusWeekDays, 'ru');
-} else {
-  getApis(engText, engWeekDays, 'en');
-}
 
-async function getCoord() {
-  const city = 'Vitebsk';
+  const latCoord = document.querySelector('.lat');
+  const lngCoord = document.querySelector('.lng');
+  const getCoordinates = await getCoord(cityRequest);
+  const latitude = getCoordinates[0];
+  const longitude = getCoordinates[1];
+  latCoord.innerHTML = `${text[5]}: ${latitude.toFixed(2)}&deg`;
+  lngCoord.innerHTML = `${text[6]}: ${longitude.toFixed(2)}&deg`;
+}
+async function checkLang(city) {
+  if (lang === 'ru') {
+    getApis(await city, rusText, rusWeekDays, 'ru');
+  } else {
+    getApis(await city, engText, engWeekDays, 'en');
+  }
+}
+checkLang(getLocation());
+
+async function getCoord(searchOptions) {
   const keyGeo = 'c6b6da0f80f24b299e08ee1075f81aa5';
-  const urlGeo = `https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${keyGeo}&pretty=1&limit=1`;
+  const urlGeo = `https://api.opencagedata.com/geocode/v1/json?q=${searchOptions}&key=${keyGeo}&pretty=1&limit=1`;
   const geoResponce = await fetch(urlGeo);
   const geoData = await geoResponce.json();
   let lat = geoData.results[0].geometry.lat;
@@ -189,4 +223,4 @@ async function getMap(coord, language) {
   });
   return map;
 }
-getMap(getCoord(), lang);
+getMap(getCoord(cityRequest), lang);
